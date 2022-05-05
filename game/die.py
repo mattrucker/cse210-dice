00896fd7f09d34d8f2086cf57968f4b60d68f71f
elohel <matt.rucker@gmail.com>
2022-05-04 19:10:24 -0500

    Created class "Die" according to specificiation. Tested and verified working.

--- game/die.py
+++ game/die.py
@@ -1,29 +1,35 @@
 import random
 
 
-# TODO: Implement the Die class as follows...
-
-# 1) Add the class declaration. Use the following class comment.
+class Die:
     """A small cube with a different number of spots on each of its six sides.
 
-    The responsibility of Die is to keep track of the side facing up and calculate the points for 
-    it.
-   
-    Attributes:
-        value (int): The number of spots on the side facing up.
-        points (int): The number of points the die is worth.
-    """
+        The responsibility of Die is to keep track of the side facing up and calculate the points for
+        it.
 
-# 2) Create the class constructor. Use the following method comment.
+        Attributes:
+            value (int): The number of spots on the side facing up.
+            points (int): The number of points the die is worth.
+        """
+
+    def __init__(self):
+        self.value = random.randrange(1, 6, 1)
+        self.points = ""
         """Constructs a new instance of Die with a value and points attribute.
 
-        Args:
-            self (Die): An instance of Die.
-        """
+                Args:
+                    self (Die): An instance of Die.
+                """
 
-# 3) Create the roll(self) method. Use the following method comment.
+    def roll(self):
         """Generates a new random value and calculates the points.
-        
-        Args:
-            self (Die): An instance of Die.
-        """
+
+                Args:
+                    self (Die): An instance of Die.
+                """
+        if self.value == 1:
+            self.points = 100
+        elif self.value == 5:
+            self.points = 50
+        else:
+            self.points = 0
